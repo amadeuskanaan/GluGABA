b76db8d9bd1d4d82653ed3c030e0dd5459cc4ed1
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-07 17:45:43 +0200

    initial commit

--- 001_check_spectra.py
+++ 001_check_spectra.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from glob import glob
+import glob
 from variables import *
 from utils.utils import mkdir_path
 
@@ -18,19 +18,16 @@ def check_spectra(population, workspace_dir, afs_dir, study_id, voxel_name):
         #for svs_type in ['TWIX', 'RDA']:
 
 
-        met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS_TWIX', voxel_name))
-        h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS_TWIX', '%s_w'%voxel_name))
+        met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', 'TWIX', voxel_name, voxel_name))
+        h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', 'TWIX', voxel_name, '%s_w'%voxel_name))
 
-        met_file = glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', voxel_name, '*'))
-        h20_file = glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))
-
-        print met_file
-        print h20_file
-
-        # shutil.copy(met_file, met_dir)
-        # shutil.copy(h20_file, h20_dir)
+        met_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', voxel_name, '*'))
+        h20_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))
 
         # copy svs data to local dir
+        shutil.copy(met_file, met_dir)
+        shutil.copy(h20_file, h20_dir)
+
 
 check_spectra(['BTBT'], ssri_workspace, afs_dir, 'day1', 'ACC')
 
