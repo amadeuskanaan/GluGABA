c57a2dc8de43cbcb8fc395c740adaa37af265af5
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-09 18:39:11 +0200

    initial commit

--- fpd_correction.py
+++ fpd_correction.py
@@ -5,7 +5,7 @@ from utils.utils import mkdir_path
 
 
 
-def run_frequency_phase_correction(population, workspace_dir, study_id, voxel_name):
+def run_frequency_phase_correction(population, workspace_dir, study_id, voxel_name, svs_type):
 
     for subject in population:
 
@@ -17,12 +17,18 @@ def run_frequency_phase_correction(population, workspace_dir, study_id, voxel_na
 
         # Run frequency and phase drift correction
         os.chdir(twx_dir)
-        preproc_acc = ['matlab', '-nodesktop', '-nosplash', '-noFigureWindows',
-                       '-r "run_megapressproc_auto(\'%s\') ; quit;"'%voxel_name]
+
+        if svs_type == 'PRESS':
+            preproc = ['matlab', '-nodesktop', '-nosplash', '-noFigureWindows',
+                        '-r "run_pressproc_auto.m(\'%s\') ; quit;"'%voxel_name]
+
+        elif svs_type == 'MEGA_PRESS':
+            preproc = ['matlab', '-nodesktop', '-nosplash', '-noFigureWindows',
+                       '-r "run_megapressproc_auto.m.m(\'%s\') ; quit;"' % voxel_name]
 
         #if not os.path.isfile(os.path.join(twx_xdir, voxel_name, voxel_name, '%s_diff_lcm' %voxel_name)):
         #    print ' Running Frequnency and Phase drift correction for %s' %voxel_name
-        subprocess.call(preproc_acc)
+        subprocess.call(preproc)
 
 run_frequency_phase_correction(['BTBT'], ssri_workspace, 'day1', 'ACC')
 
