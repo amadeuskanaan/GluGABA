0d94806fd6f4e574f4f55003dc66c55177704850
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-07 10:07:37 +0200

    initial commit

--- 001_check_spectra.py
+++ 001_check_spectra.py
@@ -3,7 +3,7 @@ import shutil
 import glob
 from variables import *
 from utils.utils import mkdir_path
-
+import shutil
 
 def check_spectra(population, workspace_dir, afs_dir, study_day, voxel_name, sequence):
 
@@ -21,22 +21,29 @@ def check_spectra(population, workspace_dir, afs_dir, study_day, voxel_name, seq
 
         ########## for svs_type in ['TWIX', 'RDA']:
 
-        twx_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX',  voxel_name))
-        twx_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
 
-        twx_met_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', voxel_name, '*'))[0]
-        twx_h20_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))[0]
+        twx_met_src = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', voxel_name, '*'))[0]
+        twx_h2o_src = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))[0]
 
-        rda_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA',  voxel_name))
-        rda_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA', '%s_w'%voxel_name))
+        print twx_met_src
+        print twx_h2o_src
+
+        # twx_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX',  voxel_name))
+        # twx_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
+        #
+        # os.system('cp %s %s ' % (twx_met_dir, twx_met_file))
+
+
+        # rda_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA',  voxel_name))
+        # rda_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA', '%s_w'%voxel_name))
 
 
         # rda_met_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%s.rda'%(seq,voxel_name)))[0]
         # rda_h20_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%sref.rda'%(seq,voxel_name)))[0]
 
         # copy svs data to local dir
-        os.system('cp %s %s '%(twx_met_dir, twx_met_file))
-        os.system('cp %s %s '%(twx_h20_dir, twx_h20_file))
+        # os.system('cp %s %s '%(twx_met_dir, twx_met_file))
+        # os.system('cp %s %s '%(twx_h20_dir, twx_h20_file))
         #
         # os.system('cp %s %s ' % (rda_met_dir, rda_met_file))
         # os.system('cp %s %s ' % (rda_h20_dir, rda_h20_file))
