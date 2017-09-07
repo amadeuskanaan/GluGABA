d7d4bc2074ca3dad1116dc81c90f65a9146dad3d
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-07 10:13:26 +0200

    initial commit

--- 001_check_spectra.py
+++ 001_check_spectra.py
@@ -8,7 +8,7 @@ import shutil
 def check_spectra(population, workspace_dir, afs_dir, study_day, voxel_name, sequence):
 
     for subject in population:
-
+        print '####################################################'
         # Input/outut dirs
         subject_afs = os.path.join(afs_dir, subject, study_day)
         subject_dir = mkdir_path(os.path.join(workspace_dir, subject, study_day))
@@ -26,27 +26,27 @@ def check_spectra(population, workspace_dir, afs_dir, study_day, voxel_name, seq
         twx_h2o_src = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))[0]
 
         twx_met_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX',  voxel_name))
-        twx_h20_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
+        twx_h2o_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
 
         if not os.listdir(twx_met_out):
             print 'Copying TWIX data for Subject %s voxel %s' %(subject, voxel_name)
             os.system('cp %s %s ' % (twx_met_src, twx_met_out))
-            os.system('cp %s %s ' % (twx_h2o_src, twx_h20_out))
+            os.system('cp %s %s ' % (twx_h2o_src, twx_h2o_out))
+        else:
+            print 'TWIX data for Subject %s voxel %s already copied' %(subject, voxel_name)
 
+        rda_met_src  = os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%s.rda'%(seq,voxel_name))
+        rda_h2o_src = os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%sref.rda'%(seq,voxel_name))
 
-        # rda_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA',  voxel_name))
-        # rda_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA', '%s_w'%voxel_name))
+        rda_met_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA',  voxel_name))
+        rda_h2o_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA', '%s_w'%voxel_name))
 
-
-        # rda_met_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%s.rda'%(seq,voxel_name)))[0]
-        # rda_h20_file = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'RDA', '%s%sref.rda'%(seq,voxel_name)))[0]
-
-        # copy svs data to local dir
-        # os.system('cp %s %s '%(twx_met_dir, twx_met_file))
-        # os.system('cp %s %s '%(twx_h20_dir, twx_h20_file))
-        #
-        # os.system('cp %s %s ' % (rda_met_dir, rda_met_file))
-        # os.system('cp %s %s ' % (rda_h20_dir, rda_h20_file))
+        if not os.listdir(rda_met_out):
+            print 'Copying RDA data for Subject %s voxel %s' % (subject, voxel_name)
+            os.system('cp %s %s' %(rda_met_src, rda_met_out))
+            os.system('cp %s %s' %(rda_h2o_src, rda_h2o_out))
+        else:
+            print 'RDA data for Subject %s voxel %s already copied' %(subject, voxel_name)
 
 check_spectra(['KA3X'], ssri_workspace, afs_dir, 'day1', 'ACC', 'PRESS')
 check_spectra(['KA3X'], ssri_workspace, afs_dir, 'day1', 'M1', 'PRESS')
