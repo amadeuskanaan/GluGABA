397bed90d98968ca9aea33373ea33e7ac4e2ee51
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-07 10:08:47 +0200

    initial commit

--- 001_check_spectra.py
+++ 001_check_spectra.py
@@ -25,13 +25,11 @@ def check_spectra(population, workspace_dir, afs_dir, study_day, voxel_name, seq
         twx_met_src = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', voxel_name, '*'))[0]
         twx_h2o_src = glob.glob(os.path.join(subject_afs, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name, '*'))[0]
 
-        print twx_met_src
-        print twx_h2o_src
+        twx_met_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX',  voxel_name))
+        twx_h20_out  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
 
-        # twx_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX',  voxel_name))
-        # twx_h20_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'TWIX', '%s_w'%voxel_name))
-        #
-        # os.system('cp %s %s ' % (twx_met_dir, twx_met_file))
+        os.system('cp %s %s ' % (twx_met_src, twx_met_out))
+        os.system('cp %s %s ' % (twx_h2o_src, twx_h20_out))
 
 
         # rda_met_dir  = mkdir_path(os.path.join(subject_dir, 'SVS', voxel_name, 'RDA',  voxel_name))
